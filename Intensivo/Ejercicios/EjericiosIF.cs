4198515ef035de57d951fd40e22f68b9e3dc242e
Juan Pablo Rodriguez Toro <44555206+Aire19527@users.noreply.github.com>
2023-03-23 18:57:22 -0500

    Agregar archivos de proyecto.

---
+++ Intensivo/Ejercicios/EjericiosIF.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intensivo.Ejercicios
+{
+    public class EjericiosIF
+    {
+
+        //Algoritmo que pida dos números e indique si el primero es mayor que el segundo o no.
+        public void NumeroMayor()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("***Numero Mayor********");
+            Console.WriteLine("***********************");
+            Console.WriteLine("Por favor escriba el número 1.");
+            int numero_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Por favor escriba el número 2.");
+            int numero_2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero_1 > numero_2)
+            {
+                Console.WriteLine("Felicidades, el número 1, es mayor que el número 2");
+                Console.WriteLine("");
+                Console.WriteLine($"Felicidades, el número: {numero_1}, es mayor que el número: {numero_2}");
+            }
+            else
+            {
+                Console.WriteLine("No se cumplió la condición, el número 2 es mayor que el número 1");
+                Console.WriteLine("");
+                Console.WriteLine($"No se cumplió la condición, el número: {numero_2}, es mayor que el número: {numero_1}");
+            }
+        }
+
+        //Algoritmo que pida un número y diga si es positivo, negativo o 0.
+        public void PositivoNegativo()
+        {
+            Console.WriteLine("****************************************");
+            Console.WriteLine("***Numero Positivo, Negativo ó 0********");
+            Console.WriteLine("****************************************");
+            Console.WriteLine("Por favor escriba el número.");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Console.WriteLine($"El número: {numero} es un número Positivo");
+            }
+            else if (numero < 0)
+            {
+                Console.WriteLine($"El número: {numero} es un número Negativo");
+            }
+            else
+            {
+                Console.WriteLine($"El número: {numero} es 0");
+            }
+
+        }
+
+        //Escribe un programa que lea un número e indique si es par o impar.
+        public void ParImpar()
+        {
+            Console.WriteLine("****************************");
+            Console.WriteLine("***Numero Paró Impar********");
+            Console.WriteLine("****************************");
+            Console.WriteLine("Por favor escriba el número.");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            double result = numero % 2;
+
+            if (result == 0)
+            {
+                Console.WriteLine($"El número: {numero} es un número Par");
+            }
+            else
+            {
+                Console.WriteLine($"El número: {numero} es un número Impar");
+            }
+        }
+    }
+}
---
+++ Intensivo/Program.cs
@@ -0,0 +1,69 @@
+﻿using Intensivo.Ejercicios;
+using System;
+
+namespace Intensivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Tipos de datos númericos
+            //int numeroEntero = 10;
+            //double numeroDouble = 12.52;
+            //float numeroFloat = 14;
+            //decimal numeroDecimal = 16;
+
+
+            ////tipo de datos texto
+            //string tipoTexto = "Hola mundo del gallinero x2";
+
+            ////Tipos boleanos
+            //bool resultado = false;//true
+
+            //Console.WriteLine(tipoTexto);
+            //int resultaMultiplicacion = numeroEntero * 50;
+
+            //string forma_1 = "Resultado: " + resultaMultiplicacion;
+            //Console.WriteLine(forma_1);
+
+            //string forma_2 = $"Resultado: {resultaMultiplicacion}";
+            //Console.WriteLine(forma_2);
+
+            //NumeroMayor();
+
+
+            EjericiosIF objEjercicio= new EjericiosIF();
+            objEjercicio.ParImpar();
+        }
+
+
+        //Algoritmo que pida dos números e indique si el primero es mayor que el segundo o no.
+        private static void NumeroMayor()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("***Numero Mayor********");
+            Console.WriteLine("***********************");
+            Console.WriteLine("Por favor escriba el número 1.");
+            int numero_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Por favor escriba el número 2.");
+            int numero_2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero_1 > numero_2)
+            {
+                Console.WriteLine("Felicidades, el número 1, es mayor que el número 2");
+                Console.WriteLine("");
+                Console.WriteLine($"Felicidades, el número: {numero_1}, es mayor que el número: {numero_2}");
+            }
+            else
+            {
+                Console.WriteLine("No se cumplió la condición, el número 2 es mayor que el número 1");
+                Console.WriteLine("");
+                Console.WriteLine($"No se cumplió la condición, el número: {numero_2}, es mayor que el número: {numero_1}");
+            }
+        }
+    
+        
+    
+    }
+}
